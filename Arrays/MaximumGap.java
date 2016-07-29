87407137218d63f389b90d500f8ded575cd67dba
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-07-29 10:43:55 -0500

    New version

--- Arrays/MaximumGap.java
+++ Arrays/MaximumGap.java
@@ -4,7 +4,7 @@ public class MaximumGap {
 
 	public static void main(String[] args) {
 		int[] nums = new int[]{4,1,7,5,2,4,21};
-		System.out.println("The maximum gap is: " + maximumGap(nums));
+		System.out.println("The maximum gap is: " + maximumGap1(nums));
 	}
 
 	public static int maximumGap(int[] nums) {
@@ -47,4 +47,39 @@ public class MaximumGap {
 		}
 		return gap;
 	}//Radix sort, complexity O(10n) == O(n);
+
+	public static int maximumGap1(int[] nums) {
+		int n = nums.length;
+		if(n < 2) return 0;
+
+		int min = nums[0];
+		int max = nums[0];
+
+		for(int i = 1;i < n;i++){
+			if(min > nums[i]) min = nums[i];
+			if(max < nums[i]) max = nums[i];
+		}
+
+		int gap = (max-min)/(n-1);
+		if(gap == 0) gap++;
+		int len = (max-min)/gap+1;
+
+		int [] tmax = new int [len];
+		int [] tmin = new int [len];
+
+		for(int i = 0;i < n;i++){
+			int index = (nums[i]-min)/gap;
+
+			if(nums[i] > tmax[index]) tmax[index] = nums[i];
+			if(tmin[index] == 0 || nums[i] < tmin[index]) tmin[index] = nums[i];
+		}
+
+		int myMax = 0;
+
+		for(int i = 0;i < len;i++){
+			if(myMax < tmin[i]-min) myMax = tmin[i]-min; //Gap between current max and previous min.
+			if(tmax[i] != 0) min = tmax[i];//new max;
+		}
+		return myMax;
+	}//bucket sort, better than radix sort.
 }
