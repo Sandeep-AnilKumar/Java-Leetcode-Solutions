c29e6e6d0c230d6ef7e67a48d439addf23b068a5
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-07-29 12:26:57 -0500

    New revision

--- DynamicProgramming/LongestIncreasingSubsequence1.java
+++ DynamicProgramming/LongestIncreasingSubsequence1.java
@@ -25,4 +25,53 @@ public class LongestIncreasingSubsequence1 {
 
 		return len;
 	}
+
+	/*
+	 * public static int longestIncreasingSubSequence(int[] nums) {
+		if(nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int len = 0;
+		int length = nums.length;
+		int j = 0;
+		int[] dp = new int[length];
+		for(int i = 0; i < length; ++i) {
+			j = binarySearch(dp, 0, len, nums[i]);
+			if(j < 0) {
+				j = -(j + 1);
+			}
+
+			dp[j] = nums[i];
+
+			if(j == len) {
+				len++;
+			}
+		}
+		//sequence of numbers is in dp, till you encounter the first zero;
+		return len;
+	}
+
+	public static int binarySearch(int[] array, int low, int high, int num) {
+		if(low > high) {
+			return -1;
+		}
+
+		high = high - 1;
+		int mid = 0;
+		while(low <= high) {
+			mid = low + (high - low)/2;
+			if(array[mid] == num) {
+				return mid;
+			}
+			else if(array[mid] < num){
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		return -(low + 1);
+	}
+	 * */
 }
