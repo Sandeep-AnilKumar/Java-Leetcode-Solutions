2727df9ade2901e6290665751c52165d61a199e4
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-08-01 12:41:58 -0500

    New version

--- Arrays/FindMinimumInRotatedSortedArray2.java
+++ Arrays/FindMinimumInRotatedSortedArray2.java
@@ -8,7 +8,7 @@ public class FindMinimumInRotatedSortedArray2 {
 	public static void main(String[] args) {
 		int[] nums = new int[]{1,1,3};
 		int k = 3;
-		int result = findMin(nums);
+		int result = findMin1(nums);
 		System.out.println("Minimum element is: " + result);
 	}
 
@@ -66,5 +66,45 @@ public class FindMinimumInRotatedSortedArray2 {
 		return newArray[low];
 	} //Brute Force, not a good technique.
 
+	//A better solution
 
+	public static int findMin1(int[] nums) {
+		if(nums == null || nums.length == 0) {
+			return 0;
+		}
+		int length = nums.length;
+
+		if(length == 1) {
+			return nums[0];
+		}
+
+		int low = 0;
+		int high = nums.length - 1;
+		int mid = 0;
+
+		while(low < high) {
+			mid = low + ((high - low ) >>> 1);
+
+			if(nums[mid] == nums[high]) {
+				if(nums[mid] == nums[low]) {
+					if(mid > 0 && nums[mid] != nums[mid - 1]) {
+						high = mid;
+					}
+					else {
+						low = mid + 1;
+					}
+				}
+				else {
+					high = mid;
+				}
+			}
+			else if(nums[mid] < nums[high]) {
+				high = mid;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+		return nums[low];
+	}
 }
