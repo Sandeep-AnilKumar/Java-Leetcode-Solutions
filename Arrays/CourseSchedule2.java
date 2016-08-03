10aaad28fb7ce8ffae40c6e3930de1cc804afdcb
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-08-03 00:26:01 -0500

    New version

--- Arrays/CourseSchedule2.java
+++ Arrays/CourseSchedule2.java
@@ -6,65 +6,91 @@ import java.util.Queue;
 public class CourseSchedule2 {
 
 	public static void main(String[] args) {
-		int prerequisites[][] = new int[][]{{1,0},{2,0}};
-		int ordering[] = findOrdering(3, prerequisites);
-		for(int i = 0; i < ordering.length; i++) {
-			System.out.println(ordering[i]);
+		int prerequisites[][] = new int[][]{{1,0},{2,0},{3,1},{3,2}};
+		int coursesToFinish = 4;
+		boolean canFinish = canFinish(coursesToFinish, prerequisites);
+		if(canFinish) {
+			System.out.println("The courses can be finished, and the order is: -");
+			int ordering[] = findOrder(coursesToFinish, prerequisites);
+			for(int i = 0; i < ordering.length; i++) {
+				System.out.print(ordering[i] + ", ");
+			}
+		}
+		else {
+			System.out.println("The courses cannot be finished");
 		}
 	}
 
-	public static int[] findOrdering(int numCourses, int[][] prerequisites) {
-		int ordering[] = new int[numCourses];
-		if(numCourses <= 0) {
-			return ordering;
+	public static boolean canFinish(int numCourses, int[][] prerequisites) {
+		int length = prerequisites.length;
+		int[] inDegree = new int[numCourses];
+
+		for(int i = 0; i < length; ++i) {
+			inDegree[prerequisites[i][0]]++;
 		}
 
-		if(prerequisites == null) {
-			return ordering;
-		}
-
-		int preLength = prerequisites.length;
-		if(preLength == 0)
-			return ordering;
-
-		int inDegree[] = new int[numCourses];
 		Queue<Integer> queue = new LinkedList<Integer>();
 
-		for(int i = 0; i < preLength; i++) {
-			if(!(prerequisites[i][1] > numCourses - 1)) {
-				inDegree[prerequisites[i][1]]++;
-			}
-		}
-
-		for(int i = 0; i < numCourses; i++) {
+		for(int i = 0; i < numCourses; ++i) {
 			if(inDegree[i] == 0) {
 				queue.offer(i);
 			}
 		}
 
-		int x = 0;
-		int j = 0;
+		int cur = 0;
+		int count = 0;
 		while(!queue.isEmpty()) {
-			x = queue.poll();
-			for(int i = 0; i < preLength; i++) {
-				if(x == prerequisites[i][0]) {
-					inDegree[prerequisites[i][1]]--;
-					if(inDegree[prerequisites[i][1]] == 0) {
-						queue.offer(prerequisites[i][1]);
+			cur = queue.poll();
+			count++;
+			for(int p[] : prerequisites) {
+				if(p[1] == cur) {
+					inDegree[p[0]]--;
+					if(inDegree[p[0]] == 0) {
+						queue.offer(p[0]);
 					}
 				}
 			}
-			ordering[j] = x;
-			j++;
+		}
+		return count == numCourses ? true : false;
+	}
+
+
+	public static int[] findOrder(int numCourses, int[][] prerequisites) {
+		int[] ordering = new int[numCourses];
+		int length = prerequisites.length;
+		int[] inDegree = new int[numCourses];
+
+		for(int i = 0; i < length; ++i) {
+			inDegree[prerequisites[i][0]]++;
 		}
 
-		int emptyArray[] = new int[numCourses];
-		for(int i = 0; i < numCourses; i++) {
-			if(inDegree[i] != 0) {
-				return emptyArray;
+		Queue<Integer> queue = new LinkedList<Integer>();
+
+		for(int i = 0; i < numCourses; ++i) {
+			if(inDegree[i] == 0) {
+				queue.offer(i);
 			}
+		}
+
+		int j = 0;
+		int cur = 0;
+		while(!queue.isEmpty()) {
+			cur = queue.poll();
+			ordering[j++] = cur;
+
+			for(int p[] : prerequisites) {
+				if(p[1] == cur) {
+					inDegree[p[0]]--;
+					if(inDegree[p[0]] == 0) {
+						queue.offer(p[0]);
+					}
+				}
+			}
+		}
+
+		if(numCourses != j) {
+			return new int[0];
 		}
 		return ordering;
 	}
-
 }
