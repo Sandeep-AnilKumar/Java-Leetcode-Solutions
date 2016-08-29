f81a182407ec75abf2e6f823d12855375441d52c
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-08-29 02:03:48 -0500

    New Code.

---
+++ src/Trees/ValidBST.java
@@ -0,0 +1,30 @@
+package Trees;
+
+public class ValidBST {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode rootLeft = new TreeNode(6);
+        TreeNode rootRight = new TreeNode(7);
+
+        root.right = rootRight;
+        root.left = rootLeft;
+        System.out.println(isValidBST(root));
+    }
+
+
+    public static boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+
+        boolean result;
+        if((root.left != null && root.val < root.left.val) || (root.right != null && root.right.val < root.val)) {
+            return false;
+        }
+        else {
+            result = isValidBST(root.left) && isValidBST(root.right);
+        }
+        return result;
+    }
+}
