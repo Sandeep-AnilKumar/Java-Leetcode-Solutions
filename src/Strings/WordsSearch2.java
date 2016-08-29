f0ea99a10191346d7d03f2a6d497bf9a93e327c9
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-08-28 21:13:25 -0500

    New version.

--- src/Strings/WordsSearch2.java
+++ src/Strings/WordsSearch2.java
@@ -1,6 +1,9 @@
 package Strings;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class WordsSearch2 {
 
@@ -61,5 +64,46 @@ public class WordsSearch2 {
             }
         }
         return found;
+    }// Might lead to TLE.
+
+    //A better solution using Trie.
+
+    static Set<String> res = new HashSet<String>();
+
+    public static List<String> findWords1(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, visited, "", i, j, trie);
+            }
+        }
+
+        return new ArrayList<String>(res);
     }
-} //might lead to TLE. 
+
+    public static void dfs(char[][] board, boolean[][] visited, String str, int x, int y, Trie trie) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
+        if (visited[x][y]) return;
+
+        str += board[x][y];
+        if (!trie.startsWith(str)) return;
+
+        if (trie.search(str)) {
+            res.add(str);
+        }
+
+        visited[x][y] = true;
+        dfs(board, visited, str, x - 1, y, trie);
+        dfs(board, visited, str, x + 1, y, trie);
+        dfs(board, visited, str, x, y - 1, trie);
+        dfs(board, visited, str, x, y + 1, trie);
+        visited[x][y] = false;
+    }
+}
