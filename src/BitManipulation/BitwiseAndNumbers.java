22d8fa9e6af2177bb6d6f3a0e2530d94f975ef49
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-24 13:11:12 -0500

    New Version.

--- src/BitManipulation/BitwiseAndNumbers.java
+++ src/BitManipulation/BitwiseAndNumbers.java
@@ -5,7 +5,7 @@ public class BitwiseAndNumbers {
     public static void main(String[] args) {
         int m = 3; //0b101
         int n = 7; //0b111
-        System.out.println(bitwiseAndOfNumberRanges1(m, n));
+        System.out.println(bitwiseAndOfNumberRanges(m, n));
         //The first difference is after 0b1, so we count till where the digits are common, and then left shift those many
         //places to give the common answer.
     }
