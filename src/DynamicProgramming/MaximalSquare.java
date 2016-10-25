f170aef0a0fb5281875949fc880581302718e118
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-24 19:33:57 -0500

    New Version.

--- src/DynamicProgramming/MaximalSquare.java
+++ src/DynamicProgramming/MaximalSquare.java
@@ -2,54 +2,49 @@ package DynamicProgramming;
 
 public class MaximalSquare {
 
-	public static void main(String[] args) {
-		char [][]array = new char[][]{{'1','0','0','0','1'},
-			{'1','0','1','1','1'},
-			{'1','0','1','1','1'},
-			{'1','1','1','1','1'},
-			{'1','1','1','1','1'}};
+    public static void main(String[] args) {
+        char [][]array = new char[][]{{'1','0','0','0','1'},
+            {'1','0','1','1','1'},
+            {'1','0','1','1','1'},
+            {'1','1','1','1','1'},
+            {'1','1','1','1','1'}};
 
-			int area = maximalSqaure(array);
-			System.out.println("The area of the maximum sqaure is :"+ area);
-	}
+            int area = maximalSquare(array);
+            System.out.println("The area of the maximum sqaure is :"+ area);
+    }
 
-	public static int maximalSqaure(char[][] matrix)
-	{
-		int rowSize = matrix.length;
-		int colSize = matrix[0].length;
-		int maxArea = 0;
+    public static int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0) {
+            return 0;
+        }
 
-		int [][]countMatrix = new int[rowSize][colSize];
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-		for(int i = 0; i < countMatrix.length; i++)
-		{	
-			if(matrix[i][0] == '1')
-			{
-				countMatrix[i][0] = 1;
-				maxArea = 1;
-			}
-		}
-		for(int j = 0; j < countMatrix[0].length; j++)
-		{	
-			if(matrix[0][j] == '1')
-			{
-				countMatrix[0][j] = 1;
-				maxArea = 1;
-			}	
-		}
+        int[][]dp = new int[row][col];
+        int maxArea = 0;
+        for(int i = 0; i < row; ++i) {
+            if(matrix[i][0] == '1') {
+                dp[i][0] = matrix[i][0] - '0';
+                maxArea = dp[i][0];
+            }
+        }
 
-		for(int i = 1; i<countMatrix.length;i++)
-		{
-			for(int j = 1; j<countMatrix[0].length;j++)
-			{	
-				if(matrix[i][j] == '1')
-				{
-					countMatrix[i][j] = Math.min(countMatrix[i-1][j-1], 
-							Math.min(countMatrix[i][j-1],countMatrix[i-1][j])) + 1;
-					maxArea = Math.max(maxArea, countMatrix[i][j]);
-				}
-			}
-		}
-		return maxArea*maxArea;
-	}
+        for(int i = 0; i < col; ++i) {
+            if(matrix[0][i] == '1') {
+                dp[0][i] = matrix[0][i] - '0';
+                maxArea = dp[0][i];
+            }
+        }
+
+        for(int i = 1; i < row; ++i) {
+            for(int j = 1; j < col; ++j) {
+                if(matrix[i][j] - '0'== 1) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    maxArea = Math.max(maxArea, dp[i][j]*dp[i][j]);
+                }
+            }
+        }
+        return maxArea;
+    }
 }
