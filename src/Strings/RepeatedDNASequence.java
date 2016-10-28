44cde216a5b24e41926ccce367ba281d2dbb42e6
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-28 18:39:03 -0500

    New Version.

--- src/Strings/RepeatedDNASequence.java
+++ src/Strings/RepeatedDNASequence.java
@@ -1,13 +1,15 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RepeatedDNASequence {
 
     public static void main(String[] args) {
-        String s = "AAAAAAAAAA";
-        System.out.println(findRepeatedDnaSequences(s));
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        System.out.println(findRepeatedDNASequences1(s));
     }
 
     //TLE version, Brute Force.
@@ -25,14 +27,39 @@ public class RepeatedDNASequence {
         int j = 0;
         for(int i = 0; i + 10 < length; ++i) {
             temp = s.substring(i, i + 10);
-            j = i + 1;
-            while(j < i + 10) {
-                if(s.indexOf(temp, j) >= 0 && !result.contains(temp)) {
-                    result.add(temp);
+            if(!result.contains(temp)) {
+                j = i + 1;
+                while(j < i + 10) {
+                    if(s.indexOf(temp, j) >= 0) {
+                        result.add(temp);
+                        i = j - 1;
+                        break;
+                    }
+                    j++;
                 }
-                j++;
             }
         }
         return result;
+    }
+
+    //Easier solution.
+    public static List<String> findRepeatedDNASequences1(String s) {
+        if(s == null || s.length() <= 10) {
+            return new ArrayList<String>();
+        }
+
+        Set<String> resultSet = new HashSet<>();
+        Set<String> tempSet = new HashSet<>();
+        int length = s.length();
+        String temp = "";
+        for(int i = 0; i <= length - 10; ++i) {
+            temp = s.substring(i, i + 10);
+            if(tempSet.contains(temp)) {
+                resultSet.add(temp);
+            } else {
+                tempSet.add(temp);
+            }
+        }
+        return new ArrayList<String>(resultSet);
     }
 }
