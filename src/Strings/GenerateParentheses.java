dd977260f245f52ff64000406d42bd1d0abcc827
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-11-08 16:15:45 -0600

    New Version.

--- src/Strings/GenerateParentheses.java
+++ src/Strings/GenerateParentheses.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class GenerateParentheses {
 
     public static void main(String[] args) {
-        int n = 25;
+        int n = 3;
         System.out.println(generateParenthesis(n));
     }
 
@@ -29,5 +29,32 @@ public class GenerateParentheses {
         }
         recurse(res, "", n, n);
         return res;
+    }
+
+    //Using StringBuilder.
+
+    public static List<String> generateParenthesisStringBuilder(int n) {
+        List<String> result = new ArrayList<>();
+        if(n <= 0) {
+            return result;
+        }
+
+        recurse(result, new StringBuilder(), n, n);
+        return result;
+    }
+
+    public static void recurse(List<String> result, StringBuilder sb, int left, int right) {
+        if(right == 0) {
+            result.add(sb.toString());
+            return;
+        }
+        if(left > 0) {
+            recurse(result, sb.append("("), left - 1, right);
+            sb.setLength(Math.max(sb.length() - 1, 0));
+        }
+        if(right > left) {
+            recurse(result, sb.append(")"), left, right - 1);
+            sb.setLength(Math.max(sb.length() - 1, 0));
+        }
     }
 }
