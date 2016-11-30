56e0e3d92e52e67f10d981e5d36bf7d7fab94c1e
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-11-30 01:05:06 -0600

    New Version.

--- src/Strings/FindAndReplace.java
+++ src/Strings/FindAndReplace.java
@@ -3,16 +3,26 @@ package Strings;
 public class FindAndReplace {
 
     public static void main(String[] args) {
-        String s = "This film is the best film of the filmy year";
+        String s = "This film is the best film of this filmy year";
         String toReplace = "film";
         String replaceWith = "movie";
-        System.out.println("String is := " + s);
+        boolean wholeWord = false;
+        System.out.println("String is := '" + s + "'");
         System.out.println("Word to be replaced is := '" + toReplace + "' with := '" + replaceWith + "'");
-        String result = replaceString(s, toReplace, replaceWith);
-        System.out.println("The replaced string is := " + result);
+        String result = replaceString(s, toReplace, replaceWith, wholeWord);
+        System.out.println("The replaced string is := '" + result + "'\n"
+                + "with whole word replacement as '" + wholeWord + "'");
+
+        System.out.println("\n--------------------------------------------------------------------\n");
+        wholeWord = true;
+        System.out.println("String is := '" + s + "'");
+        System.out.println("Word to be replaced is := '" + toReplace + "' with := '" + replaceWith + "'");
+        result = replaceString(s, toReplace, replaceWith, wholeWord);
+        System.out.println("The replaced string is := '" + result + "'\n"
+                + "with whole word replacement as '" + wholeWord + "'");
     }
 
-    public static String replaceString(String s, String toReplace, String replaceWith) {
+    public static String replaceString(String s, String toReplace, String replaceWith, boolean wholeWord) {
         if(s == null || toReplace == null || replaceWith == null ||
                 s.length() == 0 || toReplace.length() == 0 || replaceWith.length() == 0) {
             return s;
@@ -20,7 +30,10 @@ public class FindAndReplace {
 
         char[] sChars = s.toCharArray();
         int length = sChars.length;
-        char startChar = toReplace.charAt(0);
+        char startChar = wholeWord ? ' ' : toReplace.charAt(0);
+
+        if(wholeWord) toReplace = ' ' + toReplace + ' ';
+
         char[] replaceWithChars = replaceWith.toCharArray();
 
         StringBuilder sb = new StringBuilder();
@@ -28,28 +41,23 @@ public class FindAndReplace {
 
         for(int i = 0; i < length; ++i) {
             if(sChars[i] == startChar && checkIsWord(sChars, toReplace, i + 1)) {
-                for(char r : replaceWithChars) {
-                    sb.append(r);
-                }
+                if(wholeWord) sb.append(' ');
+                for(char r : replaceWithChars) sb.append(r);
+                if(wholeWord) sb.append(' ');
                 i += replaceLength - 1;
-            } else {
-                sb.append(sChars[i]);
-            }
+
+            } else sb.append(sChars[i]);
         }
         return sb.toString();
     }
 
     public static boolean checkIsWord(char[] sChars, String toReplace, int index) {
-        if(index >= sChars.length) {
-            return false; 
-        }
+        if(index >= sChars.length) return false; 
 
         char[] replace = toReplace.toCharArray();
 
         for(int i = index, j = 1; i < sChars.length && j < replace.length; ++i, ++j) {
-            if(sChars[i] != replace[j]) {
-                return false;
-            }
+            if(sChars[i] != replace[j]) return false;
         }
         return true;
     }
