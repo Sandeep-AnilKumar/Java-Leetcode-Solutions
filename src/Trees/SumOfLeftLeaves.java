a9f8efe296afd715b79dea07a704bb3489dc07aa
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-12-02 16:36:36 -0600

    New Version.

--- src/Trees/SumOfLeftLeaves.java
+++ src/Trees/SumOfLeftLeaves.java
@@ -2,6 +2,8 @@ package Trees;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SumOfLeftLeaves {
 
@@ -51,5 +53,24 @@ public class SumOfLeftLeaves {
             }
         }
         return sum;
-    }   
+    }
+
+    //With using a single queue.
+
+    public static int sumOfLeftLeavesBetter(TreeNode root) {
+        if(root == null || root.left == null && root.right == null) return 0;
+
+        int res = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+
+            if(curr.left != null && curr.left.left == null && curr.left.right == null) res += curr.left.val;
+            if(curr.left != null) queue.offer(curr.left);
+            if(curr.right != null) queue.offer(curr.right);
+        }
+        return res;
+    }
 }
