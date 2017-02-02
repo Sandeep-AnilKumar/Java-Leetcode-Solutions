0414270f170163745097b1ccda3d5c0205b8465d
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2017-02-02 06:49:22 -0600

    New Version.

--- src/BitManipulation/HammingDistance.java
+++ src/BitManipulation/HammingDistance.java
@@ -51,4 +51,14 @@ public class HammingDistance {
 
         return sb.reverse().toString();
     }
+
+    //A better solution.
+
+    public static int hammingDistanceBetter(int x, int y) {
+        int xor = x ^ y, count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += (xor >> i) & 1;
+        }
+        return count;
+    }
 }
