560967d869c24d2bfe1297fe671a63c003f8c4cd
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2017-02-02 04:39:12 -0600

    New code.

---
+++ src/Strings/LargestProductPalindrome.java
@@ -0,0 +1,63 @@
+package Strings;
+
+public class LargestProductPalindrome {
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(largestPalindrome(n));
+    }
+
+    //Brute force, does not work for all cases.
+    public static int largestPalindrome(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        int temp = n;
+        while(temp-- > 0) {
+            sb.append('9');
+        }
+
+        int num1 = Integer.parseInt(sb.toString());
+        int num2 = num1;
+        int prod = 0;
+        boolean found = false;
+
+        while(num1 > 0) {
+            num2 = num1;
+            while(num2 > 0) {
+                prod = num1*num2;
+                if(ifIsPalindrome(prod)) {
+                    found = true;
+                    break;
+                }
+                num2--;
+            }
+            if(found) {
+                break;
+            }
+            num1--;
+        }
+        prod %= 1337;
+        return prod;
+    }
+
+    public static boolean ifIsPalindrome(int prod) {
+        if(prod <= 0) {
+            return false;
+        }
+
+        char[] p = String.valueOf(prod).toCharArray();
+
+        int i = 0;
+        int j = p.length - 1;
+
+        while(i <= j) {
+            if(p[i++] != p[j--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
