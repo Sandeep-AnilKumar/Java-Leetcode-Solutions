c95cd886b84d25abe83c7c617e3edf48e441d8df
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-03-24 16:35:51 -0700

    New Version(506, Google)

--- src/Google/RelativeRanks.java
+++ src/Google/RelativeRanks.java
@@ -42,4 +42,63 @@ public class RelativeRanks {
 		}
 		return result;	
 	}
+
+	//A better version.
+	public static String[] findRelativeRanksBetter(int[] nums) {
+		int[][] pair = new int[nums.length][2];
+
+		for (int i = 0; i < nums.length; i++) {
+			pair[i][0] = nums[i];
+			pair[i][1] = i;
+		}
+
+		Arrays.sort(pair, (a, b) -> (b[0] - a[0]));
+
+		String[] result = new String[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0) {
+				result[pair[i][1]] = "Gold Medal";
+			}
+			else if (i == 1) {
+				result[pair[i][1]] = "Silver Medal";
+			}
+			else if (i == 2) {
+				result[pair[i][1]] = "Bronze Medal";
+			}
+			else {
+				result[pair[i][1]] = (i + 1) + "";
+			}
+		}
+		return result;
+	}
+
+	//Best version.
+	public static String[] findRelativeRanksBest(int[] nums) {
+		Integer[] index = new Integer[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			index[i] = i;
+		}
+
+		Arrays.sort(index, (a, b) -> (nums[b] - nums[a]));
+
+		String[] result = new String[nums.length];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0) {
+				result[index[i]] = "Gold Medal";
+			}
+			else if (i == 1) {
+				result[index[i]] = "Silver Medal";
+			}
+			else if (i == 2) {
+				result[index[i]] = "Bronze Medal";
+			}
+			else {
+				result[index[i]] = (i + 1) + "";
+			}
+		}
+		return result;
+	}
 }
