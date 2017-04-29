31f67adb5d5b2fc91a3df18579a00d3196231ca8
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-04-29 14:50:42 -0700

    New Version(252, Meeting Rooms)

--- src/Arrays/MeetingRooms.java
+++ src/Arrays/MeetingRooms.java
@@ -1,7 +1,7 @@
 package Arrays;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MeetingRooms {
 	class Interval {
@@ -22,24 +22,46 @@ public class MeetingRooms {
 	public static void main(String[] args) {
 		Interval[] intervals = new Interval[3];
 		MeetingRooms m = new MeetingRooms();
-		intervals[0] = m.new Interval(0, 30);
-		intervals[1] = m.new Interval(5, 10);
+		intervals[0] = m.new Interval(5,6);
+		intervals[1] = m.new Interval(6,8);
 		intervals[2] = m.new Interval(15,20);
-		System.out.println("Can I attend all the meetings? := " + m.canAttendMeetings(intervals));
+		System.out.println("Can I attend all the meetings? := " + m.canAttendMeetingsBetter(intervals));
 	}
 
+	//Will fail for huge numbers.
 	public boolean canAttendMeetings(Interval[] intervals) {
 		if(intervals == null || intervals.length <= 1) {
 			return true;
 		}
-		Set<Integer> blocked = new HashSet<>();
+		int[] blocked = new int[998119];
 
 		for(Interval interval: intervals) {
 			for(int time = interval.start; time < interval.end; ++time) {
-				if(blocked.contains(time)) {
+				if(blocked[time] != 0) {
 					return false;
 				}
-				blocked.add(time);
+				blocked[time] = 1;
+			}
+		}
+		return true;
+	}
+
+
+	public boolean canAttendMeetingsBetter(Interval[] intervals) {
+		if(intervals == null || intervals.length <= 1) {
+			return true;
+		}
+
+		Arrays.sort(intervals, new Comparator<Interval>(){
+			public int compare(Interval i1, Interval i2) {
+				return i1.start - i2.start;
+			} 
+		});
+
+		int length = intervals.length;
+		for(int i = 1; i < length; ++i) {
+			if(intervals[i].start < intervals[i-1].end) {
+				return false;
 			}
 		}
 		return true;
