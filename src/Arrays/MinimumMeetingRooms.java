6b243c607c7f39a495a3c2c872455da284e2b3b9
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-04-29 16:03:00 -0700

    New Version (253, Meeting Rooms)

--- src/Arrays/MinimumMeetingRooms.java
+++ src/Arrays/MinimumMeetingRooms.java
@@ -2,18 +2,20 @@ package Arrays;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 import Arrays.MeetingRooms.Interval;
 
 public class MinimumMeetingRooms {
 
 	public static void main(String[] args) {
-		Interval[] intervals = new Interval[3];
+		Interval[] intervals = new Interval[4];
 		MeetingRooms m = new MeetingRooms();
-		intervals[0] = m.new Interval(5,6);
-		intervals[1] = m.new Interval(6,8);
-		intervals[2] = m.new Interval(8,9);
-		System.out.println("Minimum number of rooms to attend all the meetings? := " + new MinimumMeetingRooms().minMeetingRooms(intervals));
+		intervals[0] = m.new Interval(9,16);
+		intervals[1] = m.new Interval(6,16);
+		intervals[2] = m.new Interval(1,9);
+		intervals[3] = m.new Interval(3,15);
+		System.out.println("Minimum number of rooms to attend all the meetings? := " + new MinimumMeetingRooms().minMeetingRoomsBetter(intervals));
 	}
 	//Will not work for all cases.
 	public int minMeetingRooms(Interval[] intervals) {
@@ -44,4 +46,39 @@ public class MinimumMeetingRooms {
 		}
 		return rooms;
 	}
+
+
+	public int minMeetingRoomsBetter(Interval[] intervals) {
+		if(intervals == null || intervals.length == 0) {
+			return 0;
+		}
+
+		int length = intervals.length;
+		Arrays.sort(intervals, new Comparator<Interval>() {
+			public int compare(Interval i1, Interval i2) {
+				return i1.start - i2.start;
+			} 
+		});
+
+		PriorityQueue<Interval> heap = new PriorityQueue<>(intervals.length, new Comparator<Interval>(){
+			public int compare(Interval i1, Interval i2) {
+				return i1.end - i2.end;
+			}
+		});
+
+
+		heap.offer(intervals[0]);
+
+		for(int i = 1; i < length; ++i) {
+			Interval interval = heap.poll();
+			if(intervals[i].start >= interval.end) {
+				interval.end = intervals[i].end;
+			} else {
+				heap.offer(intervals[i]);
+			}
+			heap.offer(interval);
+		}
+		return heap.size();
+	}
+
 }
