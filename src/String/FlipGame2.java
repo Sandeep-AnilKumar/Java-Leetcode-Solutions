06aead410a25f3b7911dee51a0a9e1112e974560
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-04-30 16:18:14 -0700

    New Version(294, Flip Game 2)

--- src/String/FlipGame2.java
+++ src/String/FlipGame2.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class FlipGame2 {
 
 	public static void main(String[] args) {
-		String s = "++++";
-		System.out.println("Can I win? := " + canWin(s));
+		String s = "+++++++++";
+		System.out.println("Can I win? := " + canWinBetter(s));
 	}
 
+	//A bit lengthy and complicated.
 	public static boolean canWin(String s) {
 		if(s == null || s.length() == 0) {
 			return false;
@@ -62,5 +63,25 @@ public class FlipGame2 {
 			}
 		}
 		return count;
+	}
+
+	//A much easier solution.
+	public static boolean canWinBetter(String s) {
+		char[] cs = s.toCharArray();
+		return canWin(cs, cs.length);
+	}
+
+	private static boolean canWin(char[] cs, int n) {
+		for (int i = 0; i < n - 1; i++) {
+			if (cs[i] == '+' && cs[i + 1] == '+') {
+				cs[i] = '-';
+				cs[i + 1] = '-';
+				boolean res = canWin(cs, n);
+				cs[i] = '+';
+				cs[i + 1] = '+';
+				if (!res) return true;
+			}
+		}
+		return false;
 	}
 }
