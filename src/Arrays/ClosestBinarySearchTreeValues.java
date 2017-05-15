b5c2508a6dee2d31bb91679c67e838d87c0f2487
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-05-14 17:59:41 -0700

    Working Solution(272, Closest BST Values)

--- src/Arrays/ClosestBinarySearchTreeValues.java
+++ src/Arrays/ClosestBinarySearchTreeValues.java
@@ -2,9 +2,13 @@ package Arrays;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 import Trees.TreeNode;
@@ -12,13 +16,13 @@ import Trees.TreeNode;
 public class ClosestBinarySearchTreeValues {
 
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(10);
-		TreeNode left = new TreeNode(5);
-		TreeNode right = new TreeNode(12);
+		TreeNode root = new TreeNode(6);
+		TreeNode left = new TreeNode(4);
+		TreeNode right = new TreeNode(8);
 		TreeNode lleft = new TreeNode(3);
-		TreeNode lright = new TreeNode(6);
-		TreeNode rleft = new TreeNode(11);
-		TreeNode rright = new TreeNode(13);
+		TreeNode lright = new TreeNode(5);
+		TreeNode rleft = new TreeNode(7);
+		TreeNode rright = new TreeNode(9);
 
 		root.left = left;
 		root.right = right;
@@ -28,7 +32,7 @@ public class ClosestBinarySearchTreeValues {
 		right.right = rright;
 
 		ClosestBinarySearchTreeValues cbstv = new ClosestBinarySearchTreeValues();
-		System.out.println(cbstv.closestKValues(root, 9.0, 4));
+		System.out.println(cbstv.closestKValuesBetter(root, 5.285714, 5));
 	}
 
 	//Will not work for Double values.
@@ -72,6 +76,49 @@ public class ClosestBinarySearchTreeValues {
 		while(k-- > 0) {
 			result.add(min.poll().val);
 		}
+		return result;
+	}
+
+	public List<Integer> closestKValuesBetter(TreeNode root, double target, int k) {
+		if(root == null || k <= 0) {
+			return new ArrayList<Integer>();
+		}
+
+		List<Integer> result = new ArrayList<>();
+		Map<Double, List<Integer>> diffMap = new HashMap<>();
+		Deque<TreeNode> stack = new ArrayDeque<>();
+		TreeNode temp = root;
+		Double diff = 0d;
+		List<Integer> tempList = null;
+
+		while(!stack.isEmpty() || temp != null) {
+			if(temp != null) {
+				stack.offerFirst(temp);
+				diff = Math.abs(temp.val - target);
+				tempList = diffMap.get(diff); 
+				if(tempList ==  null) {
+					tempList = new ArrayList<>();	
+				}
+				tempList.add(temp.val);
+				diffMap.put(diff, tempList);
+				temp = temp.left;
+			} else {
+				temp = stack.pollFirst();
+				temp = temp.right;
+			}
+		}
+
+		Double[] keys = new Double[diffMap.size()];
+		Collections.sort(Arrays.asList(diffMap.keySet().toArray(keys)));
+		int index = 0;
+		while(k > 0) {
+			tempList = diffMap.get(keys[index++]);
+			k -= tempList.size();
+			for(int val: tempList) {
+				result.add(val);
+			}
+		}
+
 		return result;
 	}
 }
