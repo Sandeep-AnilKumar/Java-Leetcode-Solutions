ff43db7b6b84b97e4859a7007065cd30f7eeb413
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-05-17 20:21:13 -0700

    New Code

---
+++ src/Arrays/MaximumProductSubarray.java
@@ -0,0 +1,53 @@
+package Arrays;
+
+public class MaximumProductSubarray {
+
+	public static void main(String[] args) {
+		int[] nums = {1, 2, -2, 4, 2, 3, 1, -1 , -10, 5};
+		System.out.println("The maximum product subarray is := " + maxProduct(nums));
+	}
+
+	public static int maxProductSubArray(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int max = nums[0], min = nums[0], result = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			int temp = max;
+			max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);	
+			min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+			if (max > result) {
+				result = max;
+			}
+		}
+		return result;
+	}
+
+	public static int maxProduct(int[] A) {
+		// edge case
+		if (A == null || A.length == 0)
+			return 0;
+
+		int max = Integer.MIN_VALUE;
+		int product = 1;
+		// first go from left to right
+		for (int i = 0; i < A.length; i++) {
+			product *= A[i];
+			if (product > max)
+				max = product;
+			if (product == 0)
+				product = 1; // reset if encounter 0
+		}
+
+		// then go from right to left
+		product = 1;
+		for (int i = A.length - 1; i >= 0; i--) {
+			product *= A[i];
+			if (product > max)
+				max = product;
+			if (product == 0)
+				product = 1; // reset if encounter 0
+		}
+		return max;
+	}
+}
---
+++ src/Arrays/QuickSortList.java
@@ -0,0 +1,63 @@
+package Arrays;
+
+public class QuickSortList {
+
+	//All the nodes having value less than the target will be on the left of the all the nodes having values greater than the target.
+	public static void main(String[] args) {
+		MyNode node = new MyNode(12);
+		MyNode a = new MyNode(1);
+		MyNode b = new MyNode(2);
+		MyNode c = new MyNode(10);
+		MyNode d = new MyNode(22);
+		MyNode e = new MyNode(4);
+		MyNode f = new MyNode(8);
+		MyNode g = new MyNode(22);
+		MyNode h = new MyNode(-1);
+		MyNode i = new MyNode(5);
+		MyNode j = new MyNode(32);
+		MyNode k = new MyNode(2);
+		MyNode l = new MyNode(7);
+
+		node.next = a;
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+		e.next = f;
+		f.next = g;
+		g.next = h;
+		h.next = i;
+		i.next = j;
+		j.next = k;
+		k.next = l;
+
+		System.out.println("The quciksorted once listnodes are := " + quickSortOnce(node, 7));
+	}
+
+	public static MyNode quickSortOnce(MyNode node, int target) {
+		if(node == null || node.next == null) {
+			return node;
+		}
+
+		MyNode before = new MyNode(0);
+		MyNode bCur = before;
+		MyNode after = new MyNode(0);
+		MyNode aCur = after;
+
+		MyNode cur = node;
+
+		while(cur != null) {
+			if(cur.val < target) {
+				bCur.next = cur;
+				bCur = bCur.next;
+			} else {
+				aCur.next = cur;
+				aCur = aCur.next;
+			}
+			cur = cur.next;
+		}
+
+		bCur.next = after.next;
+		return before.next;
+	}
+}
