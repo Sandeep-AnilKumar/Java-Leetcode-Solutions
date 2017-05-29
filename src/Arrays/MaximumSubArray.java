19b3933a81b50079b5eebe4dba05eb44cf88ce77
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-05-29 14:57:37 -0700

    New Version(53, Maximum Subarray)

--- src/Arrays/MaximumSubArray.java
+++ src/Arrays/MaximumSubArray.java
@@ -4,7 +4,7 @@ public class MaximumSubArray {
 
 	public static void main(String[] args) {
 		int nums[] = new int[]{-1,2,3-4,5,-6,-10};
-		int sum = maxSubArray(nums);
+		int sum = maxSubArray1(nums);
 		System.out.println("The maximum subarray is: "+ sum);
 	}
 
@@ -33,5 +33,27 @@ public class MaximumSubArray {
 			max = Math.max(max, dp[i]);
 		}
 		return max;
+	}
+
+	//Best solution.
+	public static int maxSubArrayBest(int[] nums) {
+		if(nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		int length = nums.length;
+		int[] dp = new int[length];
+		dp[0] = nums[0];
+		int curSum = nums[0];
+
+		for(int i = 1; i < length; ++i) {
+			if(nums[i] > curSum + nums[i]) {
+				curSum = nums[i];
+			} else {
+				curSum += nums[i];
+			}
+			dp[i] = Math.max(dp[i - 1], curSum);
+		}
+		return dp[length - 1];
 	}
 }
