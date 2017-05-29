0af6aebba4b05bbe773f496d132900c40fd30d5a
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-05-29 14:29:10 -0700

    New Code(414, Third Max Number)

--- src/Arrays/ThirdMax.java
+++ src/Arrays/ThirdMax.java
@@ -1,13 +1,16 @@
 package Arrays;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ThirdMax {
 
 	public static void main(String[] args) {
-		int[] nums = {1,1, Integer.MIN_VALUE};
-		System.out.println("The third max number is := " + thirdMax(nums));
+		int[] nums = {1,1, Integer.MIN_VALUE, 2, 2, 3, 4};
+		System.out.println("The third max number is := " + thirdMaxBetter(nums));
 	}
 
 	public static int thirdMax(int[] nums) {
@@ -44,4 +47,39 @@ public class ThirdMax {
 
 		return k > 0 ? curMax : kMax;
 	}
+
+	//A better version.
+	public static int thirdMaxBetter(int[] nums) {
+		if(nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		Set<Integer> set = new HashSet<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		for(int i: nums) {
+			if(set.add(i)) {
+				pq.offer(i);
+				if(pq.size() > 3) {
+					set.remove(pq.poll());
+				}
+			}
+		}
+
+		if(pq.size() < 3) {
+			while(pq.size() > 1) {
+				pq.poll();
+			}
+		}
+		return pq.peek();
+	}
+
+	//A much easier version.
+	public static int thirdMaxBest(int[] nums){
+		TreeSet<Integer> set = new TreeSet<>();
+		for (int num : nums)
+			if (set.add(num) && set.size() > 3)
+				set.pollFirst();
+		return set.size() > 2 ? set.pollFirst() : set.pollLast();
+	}	
 }
