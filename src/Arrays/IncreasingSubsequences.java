f15126dfd2a8e07af9f0a511d9ecc397c9e22a7e
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-05-31 10:25:55 -0700

    New Version(491, Increasing Subsequences)

--- src/Arrays/IncreasingSubsequences.java
+++ src/Arrays/IncreasingSubsequences.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,5 +36,46 @@ public class IncreasingSubsequences {
 				cur.remove(cur.size() - 1);
 			}
 		}
+	}
+
+	//A faster solution.
+	public List<List<Integer>> findSubsequencesBetter(int[] nums) {
+		ArrayList<Integer> indices = new ArrayList<>();
+		List<List<List<Integer>>> values = new ArrayList<>();
+
+		for (int num : nums) {
+			int index = Collections.binarySearch(indices, num);
+			List<Integer> solo = new ArrayList<>();
+			solo.add(num);
+
+			if (index < 0) {
+				index = -(index + 1);
+				List<List<Integer>> next = new ArrayList<>();
+				next.add(solo);
+				values.add(index, next);
+				indices.add(index, num);
+			} else {
+				for (List<Integer> list : values.get(index)) {
+					list.add(num);
+				}
+				values.get(index).add(solo);
+			}
+
+			for (int i = index - 1; i >= 0; i--) {
+				for (List<Integer> list : values.get(i)) {
+					List<Integer> next = new ArrayList<>(list);
+					next.add(num);
+					values.get(index).add(next);
+				}
+			}
+		}
+
+		List<List<Integer>> result = new ArrayList<>();
+		for (List<List<Integer>> list : values) {
+			for (List<Integer> tmp : list) {
+				if (tmp.size() > 1) result.add(tmp);
+			}
+		}
+		return result;
 	}
 }
