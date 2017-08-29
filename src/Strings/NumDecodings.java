0d6ecca4dc0e63586a290864dcb939c866758392
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-08-28 19:20:09 -0700

    New Code(91, Decode Ways)

--- src/Strings/NumDecodings.java
+++ src/Strings/NumDecodings.java
@@ -19,29 +19,48 @@ The number of ways decoding "12" is 2.
 
 public class NumDecodings {
 
-    public static void main(String[] args) {
-        String s = "12324";	
-        System.out.println("Number of decodings possible :-\t" + numDecodings(s));
-    }
+	public static void main(String[] args) {
+		String s = "26";	
+		System.out.println("Number of decodings possible :-\t" + numDecodingsBetter(s));
+	}
 
-    public static int numDecodings(String s)
-    {
-        if(s.length() == 0 || s.equals(""))
-            return 0;
-        int n = s.length();
-        int[]A = new int[n+1];
-        A[n] = 1;
-        A[n-1] = s.charAt(n-1) == '0' ? 0 : 1;
-        for(int i = n-2; i>=0 ; i--)
-        {
-            if(s.charAt(i) != '0')
-            {
-                A[i] = A[i+1];
-                int val = Integer.parseInt(s.substring(i,i+2));
-                if(val > 0 && val <= 26)
-                    A[i] += A[i+2];
-            }
-        }
-        return A[0];
-    }
+	public static int numDecodings(String s)
+	{
+		if(s.length() == 0 || s.equals(""))
+			return 0;
+		int n = s.length();
+		int[]A = new int[n+1];
+		A[n] = 1;
+		A[n-1] = s.charAt(n-1) == '0' ? 0 : 1;
+		for(int i = n-2; i>=0 ; i--)
+		{
+			if(s.charAt(i) != '0')
+			{
+				A[i] = A[i+1];
+				int val = Integer.parseInt(s.substring(i,i+2));
+				if(val > 0 && val <= 26)
+					A[i] += A[i+2];
+			}
+		}
+		return A[0];
+	}
+
+	public static int numDecodingsBetter(String s) {
+		if(s == null || s.length() == 0) return 0;
+
+		int length = s.length();
+		int[] dp = new int[length + 1];
+		dp[0] = 1;
+		dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+		for(int i = 2; i <= length; ++i) {
+			if(s.charAt(i - 1) != '0') {
+				dp[i] += dp[i - 1];
+			}
+			if(s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6')) {
+				dp[i] += dp[i - 2];
+			}
+		}
+		return dp[length];
+	}
 }
