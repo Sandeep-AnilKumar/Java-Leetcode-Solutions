2becfd1c2bf86384bb1171e900132ca07332cfac
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-01 13:46:36 -0700

    New Version(Find Error Numbers)

--- src/Arrays/FindErrorNumbers.java
+++ src/Arrays/FindErrorNumbers.java
@@ -6,8 +6,14 @@ import java.util.List;
 public class FindErrorNumbers {
 
 	public static void main(String[] args) {
-		int[] nums = {1,2,2,4};
-		System.out.println("The mssing int and duplicated one is := " + new FindErrorNumbers().findErrorNums(nums));
+		FindErrorNumbers numbers = new FindErrorNumbers();
+		int[] nums = {2,2};
+
+		System.out.println("The mssing int and duplicated one is := ");
+		int[] errors = numbers.findErrorNumsBest(nums);
+		for(int e: errors) {
+			System.out.print(e + ", ");
+		}
 	}
 
 	public int[] findErrorNums(int[] nums) {
@@ -32,5 +38,56 @@ public class FindErrorNumbers {
 			dupAndMissing[index++] = i;
 		}
 		return dupAndMissing;
+	}
+
+	//Using XOR
+
+	public int[] findErrorNumsXOR(int[] nums) {
+		int xor = 0, xor0 = 0, xor1 = 0;
+		for (int n: nums)
+			xor ^= n;
+		for (int i = 1; i <= nums.length; i++)
+			xor ^= i;
+		int rightmostbit = xor & ~(xor - 1);
+		for (int n: nums) {
+			if ((n & rightmostbit) != 0)
+				xor1 ^= n;
+			else
+				xor0 ^= n;
+		}
+		for (int i = 1; i <= nums.length; i++) {
+			if ((i & rightmostbit) != 0)
+				xor1 ^= i;
+			else
+				xor0 ^= i;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == xor0)
+				return new int[]{xor0, xor1};
+		}
+		return new int[]{xor1, xor0};
+	}
+
+	//Constant Space, easier one
+
+	public int[] findErrorNumsBest(int[] nums) {
+		int dup = 0;
+		int length = nums.length;
+		int missing = 0;
+
+		for(int i = 0; i < length; ++i) {
+			if(nums[Math.abs(nums[i]) - 1] < 0) {
+				dup = Math.abs(nums[i]);
+			} else {
+				nums[Math.abs(nums[i]) - 1] *= -1;
+			}
+		}
+
+		for(int i = 0; i < length; ++i) {
+			if(nums[i] > 0) {
+				missing = i + 1;
+			}
+		}
+		return new int[]{dup, missing};
 	}
 }
