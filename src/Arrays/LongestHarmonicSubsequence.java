c7c3fa2a13f9b9fd3caf2a500c24804e0d69f42f
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-06 16:42:21 -0700

    New Code(594, Longest Harmonic Subsequence)

---
+++ src/Arrays/LongestHarmonicSubsequence.java
@@ -0,0 +1,56 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestHarmonicSubsequence {
+
+	public static void main(String[] args) {
+		LongestHarmonicSubsequence sequence = new LongestHarmonicSubsequence();
+		int[] nums = {1,2,3,3,2,5,7,1};
+		System.out.println("The length of the longest harmonic subsequence is := ");
+		System.out.println(sequence.findLHSBetter(nums));
+	}
+
+	public int findLHS(int[] nums) {
+		if(nums == null || nums.length == 0) return 0;
+
+		Map<Integer, Integer> occ = new HashMap<>();
+		for(int i: nums) {
+			occ.put(i, occ.getOrDefault(i, 0) + 1);
+		}
+
+		int max = 0;
+
+		for(int i: nums) {
+			if(occ.containsKey(i - 1)) {
+				max = Math.max(occ.get(i) + occ.get(i - 1), max);
+			}
+
+			if(occ.containsKey(i + 1)) {
+				max = Math.max(occ.get(i) + occ.get(i + 1), max);
+			}
+		}
+		return max;
+	}
+
+	//The above solution can be optimized to run in only one loop as below:
+	public int findLHSBetter(int[] nums) {
+		if(nums == null || nums.length == 0) return 0;
+
+		Map<Integer, Integer> occ = new HashMap<>();
+		int max = 0;
+
+		for(int i: nums) {
+			occ.put(i, occ.getOrDefault(i, 0) + 1);
+			if(occ.containsKey(i - 1)) {
+				max = Math.max(occ.get(i) + occ.get(i - 1), max);
+			}
+
+			if(occ.containsKey(i + 1)) {
+				max = Math.max(occ.get(i) + occ.get(i + 1), max);
+			}
+		}
+		return max;
+	}
+}
