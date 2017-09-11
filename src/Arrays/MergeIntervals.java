bee085386c414dc97326321a13f63a522e336325
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-10 19:00:52 -0700

    New Version(Merge Intervals)

--- src/Arrays/MergeIntervals.java
+++ src/Arrays/MergeIntervals.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 class Interval {
 	int start;
@@ -36,7 +37,7 @@ public class MergeIntervals {
 		intervals.add(new Interval(5,7));
 		intervals.add(new Interval(2,2));
 		intervals.add(new Interval(4,6));
-		System.out.println("Merged Intervals are := " + new MergeIntervals().merge(intervals));
+		System.out.println("Merged Intervals are := " + new MergeIntervals().mergeBetter(intervals));
 	}
 
 	public List<Interval> merge(List<Interval> intervals) {
@@ -68,5 +69,46 @@ public class MergeIntervals {
 
 		result.add(new Interval(start, end));
 		return result;
+	}
+
+	public List<Interval> mergeBetter(List<Interval> intervals) {
+		List<Interval> merged = new ArrayList<>();
+		if(intervals == null || intervals.size() == 0) return merged;
+
+		PriorityQueue<Interval> pq = new PriorityQueue<Interval>(new Comparator<Interval>() {
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if(i1.start < i2.start) {
+					return -1;
+				} else if(i1.start > i2.start) {
+					return 1;
+				} else {
+					return i1.end - i2.end;
+				}
+			}
+		});
+
+		for(Interval i: intervals) {
+			pq.offer(i);
+		}
+
+		Interval cur = null;
+		Interval prev = null;
+
+		while(!pq.isEmpty()) {
+			cur = pq.poll();
+
+			if(merged.size() == 0) {
+				merged.add(new Interval(cur.start, cur.end));
+			} else {
+				prev = merged.get(merged.size() - 1);
+				if(cur.start <= prev.end) {
+					prev.end = Math.max(prev.end, cur.end);
+				} else {
+					merged.add(new Interval(cur.start, cur.end));
+				}
+			}
+		}
+		return merged;
 	}
 }
