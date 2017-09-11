722d5a2bb4998e6174b3c556aaf9a02c7566a2b7
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-11 15:43:08 -0700

    New Version (668, Kth Smallest Element in Multiplication Table)

--- src/Arrays/KthSmallestElementInAMultiplicationTable.java
+++ src/Arrays/KthSmallestElementInAMultiplicationTable.java
@@ -9,11 +9,11 @@ public class KthSmallestElementInAMultiplicationTable {
 		KthSmallestElementInAMultiplicationTable table = new KthSmallestElementInAMultiplicationTable();
 		int m = 3;
 		int n = 3;
-		int k = 5;
+		int k = 3;
 
-		System.out.println("The " + k + "th smallest element in the multiplication number is := " + table.findKthNumber(m, n, k));
+		System.out.println("The " + k + "th smallest element in the multiplication number is := " + table.findKthNumberBetter(m, n, k));
 	}
-	
+
 	//Not a very efficient solution for large m and n.
 	public int findKthNumber(int m, int n, int k) {
 		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
@@ -34,5 +34,31 @@ public class KthSmallestElementInAMultiplicationTable {
 			curSmall = pq.poll();
 		}
 		return curSmall;
+	}
+
+	public int findKthNumberBetter(int m, int n, int k) {
+		int low = 1, high = m * n;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			int count = helper(m, n, mid);
+			if (count >= k) high = mid - 1;
+			else low = mid + 1;
+		}
+		return low;
+	}
+
+	public int helper(int m, int n, int mid) {
+		int r = 1;
+		int c = n;
+		int count = 0;
+		while(c > 0 && r <= m) {
+			if(r * c <= mid) {
+				count += c;
+				r++;
+			} else {
+				c--;
+			}
+		}
+		return count;
 	}
 }
