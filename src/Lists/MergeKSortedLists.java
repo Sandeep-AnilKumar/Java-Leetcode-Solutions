6ff226f8df727ed2a4775c55cc68b1ea5e083b5d
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-10 17:17:24 -0700

    New Version (23, Merge k sorted lists)

--- src/Lists/MergeKSortedLists.java
+++ src/Lists/MergeKSortedLists.java
@@ -1,5 +1,10 @@
 package Lists;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class MergeKSortedLists {
 
 	public static void main(String[] args) {
@@ -113,6 +118,74 @@ public class MergeKSortedLists {
 			while(count[i]-- > 0) {
 				head.next = new ListNode(i - 500000);
 				head = head.next;
+			}
+		}
+		return merged.next;
+	}
+
+	//O(n log k) solution using priority queue. Where n is the total number of nodes in the merged list, and k is the number of lists.
+
+	public ListNode mergeKListsMuchBetter(ListNode[] lists) {
+		if (lists == null || lists.length == 0) return null;
+
+		PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(new Comparator<ListNode>(){
+			public int compare(ListNode o1,ListNode o2){
+				return o1.val - o2.val;
+			}
+		});
+
+		ListNode dummy = new ListNode(0);
+		ListNode tail=dummy;
+
+		for (ListNode node:lists)
+			if (node!=null)
+				queue.add(node);
+
+		while (!queue.isEmpty()){
+			tail.next=queue.poll();
+			tail=tail.next;
+
+			if (tail.next != null)
+				queue.add(tail.next);
+		}
+		return dummy.next;
+	}
+
+	//O(kN). So far the best solution.
+
+	public ListNode mergeKListsBest(ListNode[] lists) {
+		if(lists == null || lists.length == 0) return null;
+
+		int k = lists.length;
+		Set<Integer> active = new HashSet<>();
+
+		for(int i = 0; i < k; ++i) active.add(i);
+
+		ListNode merged = new ListNode(0);
+		ListNode head = merged;
+		int curMin = Integer.MAX_VALUE;
+		int curIndex = 0;
+		boolean found = false;
+
+		while(!active.isEmpty()) {
+			curMin = Integer.MAX_VALUE;
+			curIndex = 0;
+			found = false;
+
+			for(int i = 0; i < k; ++i) {
+				if(lists[i] != null && lists[i].val < curMin) {
+					curIndex = i;
+					curMin = lists[i].val;
+					found = true;
+				} else if(lists[i] == null) active.remove(i);
+			}
+
+			if(found) {
+				head.next = new ListNode(curMin);
+				head = head.next;
+
+				if(lists[curIndex] == null || lists[curIndex].next == null) active.remove(curIndex);
+				if(lists[curIndex] != null) lists[curIndex] = lists[curIndex].next;
 			}
 		}
 		return merged.next;
