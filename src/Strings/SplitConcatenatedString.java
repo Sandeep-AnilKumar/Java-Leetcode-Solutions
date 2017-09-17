b97f5003cc4b2cdb05e37c181e89805d5217bfd0
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-09-17 15:24:00 -0700

    New Code(555, Split Concatenated String)

---
+++ src/Strings/SplitConcatenatedString.java
@@ -0,0 +1,36 @@
+package Strings;
+
+public class SplitConcatenatedString {
+
+	public static void main(String[] args) {
+		SplitConcatenatedString split = new SplitConcatenatedString();
+		String[] strs = {"lc", "love", "ydc"};
+		System.out.println("The maximum split concatenated string is := " + split.splitLoopedString(strs));
+	}
+
+	public String splitLoopedString(String[] strs) {
+		for (int i = 0; i < strs.length; i++) {
+			String rev = new StringBuilder(strs[i]).reverse().toString();
+			if (strs[i].compareTo(rev) < 0)
+				strs[i] = rev;
+		}
+		
+		String res = "";
+		for (int i = 0; i < strs.length; i++) {
+			String rev = new StringBuilder(strs[i]).reverse().toString();
+			for (String st: new String[] {strs[i], rev}) {
+				for (int k = 0; k < st.length(); k++) {
+					StringBuilder t = new StringBuilder(st.substring(k));
+					for (int j = i + 1; j < strs.length; j++)
+						t.append(strs[j]);
+					for (int j = 0; j < i; j++)
+						t.append(strs[j]);
+					t.append(st.substring(0, k));
+					if (t.toString().compareTo(res) > 0)
+						res = t.toString();
+				}
+			}
+		}
+		return res;
+	}
+}
