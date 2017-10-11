73e6f7cff01a4826f9c4d49746e0cb17c6c21686
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-10-11 15:46:17 -0700

    New Code (Minimum Window Substring)

--- src/Strings/MinimumWindowSubstring.java
+++ src/Strings/MinimumWindowSubstring.java
@@ -1,8 +1,5 @@
 package Strings;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author sandeepa
  */
@@ -10,53 +7,34 @@ import java.util.Set;
 public class MinimumWindowSubstring {
 
 	public static void main(String[] args) {
-		String s = "ADOBEBAEFCBANC";
-		String t = "ABC";
-		
-		
-		
+		String s = "ABBCBCABAFCBBBBBACCCCC";
+		String t = "ABCA";
 		System.out.println("The minimum window substring is := " + new MinimumWindowSubstring().minWindow(s, t));
 	}
 
 	public String minWindow(String s, String t) {
 		if(s == null || s.length() == 0 || t == null || t.length() == 0) return "";
 
-		String sub = "";
-		int max = 0, l = 0, num = 0, length = s.length();
-		Set<Character> set = new HashSet<>();
+		int l = 0, length = s.length(), max = Integer.MAX_VALUE, num = t.length();
 		int[] count = new int[256];
-		char c;
-		char p;
+		int sStart = 0, sEnd = 0;
 
 		for(int i = 0; i < t.length(); ++i) {
-			set.add(t.charAt(i));
+			count[t.charAt(i)]++;
 		}
 
 		for(int i = 0; i < length; ++i) {
-			c = s.charAt(i);
-			if(set.contains(c) && count[c] == 0) {
-				num++;
-				count[c]++;
-				if(num == set.size()) {
-					if(i - l + 1 > max) {
-						max = i - l + 1;
-						sub = s.substring(l, i + 1);
-					}
-				}
-			}
+			if(count[s.charAt(i)]-- > 0) num--;
 
-			if(set.contains(c) && count[c] > 0) {
-				while(l < length && s.charAt(l) != c) {
-					p = s.charAt(l);
-					if(set.contains(p)) {
-						count[p]--;
-					}
-					l++;
+			while(num == 0) {
+				if(i - l + 1 < max) {
+					max = i - l + 1;
+					sStart = l;
+					sEnd = i;
 				}
-
-				while(l + 1 < length && !set.contains(s.charAt(l + 1))) l++;
+				if(count[s.charAt(l++)]++ == 0) num++;
 			}
 		}
-		return sub;
+		return max == Integer.MAX_VALUE ? "" : s.substring(sStart, sEnd + 1);
 	}
 }
