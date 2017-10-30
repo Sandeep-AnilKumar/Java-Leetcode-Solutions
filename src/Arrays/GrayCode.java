38eb955fb2d57e9f721656f90a31bcaaae870ac6
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-10-29 22:06:19 -0700

    New Version (Gray Code)

--- src/Arrays/GrayCode.java
+++ src/Arrays/GrayCode.java
@@ -54,4 +54,26 @@ public class GrayCode {
 		}
 		return reverse;
 	}
+
+	//No need to reverse
+
+	public List<Integer> grayCodeBetter(int n) {
+		List<Integer> list = new ArrayList<>();
+		if(n == 0) return new ArrayList<>(Arrays.asList(0));
+
+		list.add(0);
+		list.add(1);
+		int cur = 0;
+		int size = 0;
+
+		for(int i = 1; i < n; ++i) {
+			cur = 1 << i;
+			size = list.size();
+
+			for(int j = size - 1; j >= 0; --j) {
+				list.add(cur + list.get(j));
+			}
+		}
+		return list;
+	}
 }
