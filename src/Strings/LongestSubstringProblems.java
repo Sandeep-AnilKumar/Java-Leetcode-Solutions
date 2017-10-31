2f33fdb02a855d668e59072536c3361d4854581a
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-10-30 18:07:21 -0700

    New Code (Longest Repeating Character Replacement)

--- src/Strings/LongestSubstringProblems.java
+++ src/Strings/LongestSubstringProblems.java
@@ -10,6 +10,9 @@ public class LongestSubstringProblems {
 		LongestSubstringProblems longest = new LongestSubstringProblems();
 		String s = "abcaabbcda";
 		System.out.println("the length of the longest substring of string '" + s + "' without repeating characters is := " + longest.lengthOfLongestSubstring(s));
+		s = "BAAAB";
+		int k = 2;
+		System.out.println("The length of the substring with longest character replacement with '" + k + "' chars in the string '" + s + "' is := " + longest.characterReplacement(s, k));
 	}
 
 	//Longest Substring without repeating characters.
@@ -17,16 +20,32 @@ public class LongestSubstringProblems {
 		if(s == null || s.length() == 0) return 0;
 
 		int length = s.length();
-		int start = 0;
-		int max = 0;
-		char c;
 		int[] count = new int[256]; //Extended ASCII, 128 for ASCII, 26 for 'a' to 'z' or 'A' to 'Z'.
+		
+		int start = 0, max = 0;
+		char c;
 
 		for(int i = 0; i < length; ++i) {
 			c = s.charAt(i);
 			count[(int) c]++;
 
 			while(count[(int) c] > 1) count[(int) s.charAt(start++)]--;
+
+			max = Math.max(max, i - start + 1);
+		}
+		return max;
+	}
+
+	//Longest repeating character replacement
+	public int characterReplacement(String s, int k) {
+		int len = s.length();
+		int[] count = new int[256];
+		int start = 0, maxCount = 0, max = 0;
+		
+		for (int i = 0; i < len; i++) {
+			maxCount = Math.max(maxCount, ++count[(int) s.charAt(i)]);
+
+			while (i - start + 1 - maxCount > k) count[(int) s.charAt(start++)]--;
 
 			max = Math.max(max, i - start + 1);
 		}
