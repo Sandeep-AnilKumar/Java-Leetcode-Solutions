c799817aa24a05d58f1c1d78277b9f7d4340ec02
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-10-30 19:19:45 -0700

    New Version (Number of Distinct Islands)

--- src/Arrays/NumberDistinctIslands.java
+++ src/Arrays/NumberDistinctIslands.java
@@ -15,7 +15,7 @@ public class NumberDistinctIslands {
 	public static void main(String[] args) {
 		int[][] grid = {{1,1,0,1,1},{1,0,0,0,0},{0,0,0,0,1},{1,1,0,1,1}};
 		NumberDistinctIslands islands = new NumberDistinctIslands();
-		System.out.println("The number of distinct islands are := " + islands.numDistinctIslands(grid));
+		System.out.println("The number of distinct islands are := " + islands.numDistinctIslandsAlt(grid));
 	}
 
 	int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
@@ -50,5 +50,38 @@ public class NumberDistinctIslands {
 			getIslandCoordinates(originI, originJ, i + d[0], j + d[1], cur, grid);
 		}
 		return;
+	}
+
+
+	int[][] grid;
+	boolean[][] seen;
+	ArrayList<Integer> shape;
+	public void explore(int r, int c, int di) {
+		if (0 <= r && r < grid.length && 0 <= c && c < grid[0].length &&
+				grid[r][c] == 1 && !seen[r][c]) {
+			seen[r][c] = true;
+			shape.add(di);
+			explore(r+1, c, 1);
+			explore(r-1, c, 2);
+			explore(r, c+1, 3);
+			explore(r, c-1, 4);
+			shape.add(0);
+		}
+	}
+	public int numDistinctIslandsAlt(int[][] grid) {
+		this.grid = grid;
+		seen = new boolean[grid.length][grid[0].length];
+		Set<List<Integer>> shapes = new HashSet<>();
+		for (int r = 0; r < grid.length; r++) {
+			for (int c = 0; c < grid[0].length; c++) {
+				shape = new ArrayList<Integer>();
+				explore(r, c, 0);
+				if (!shape.isEmpty()) {
+					shapes.add(shape);
+				}
+			}
+		}
+		System.out.println(shapes);
+		return shapes.size();
 	}
 }
