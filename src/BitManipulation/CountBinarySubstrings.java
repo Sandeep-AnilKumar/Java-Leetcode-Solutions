eb7c882a12540e1eca0d642aeec847478541ddfb
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-11-14 00:28:50 -0800

    New Version (Count Binary Substrings)

--- src/BitManipulation/CountBinarySubstrings.java
+++ src/BitManipulation/CountBinarySubstrings.java
@@ -26,12 +26,24 @@ public class CountBinarySubstrings {
 			pre ^= 1;
 			counts.offer(curCount);
 
-			if(!counts.isEmpty() && counts.size() == 2) {
-				count += Math.min(counts.poll(), counts.peek());
-			}
+			if(!counts.isEmpty() && counts.size() == 2) count += Math.min(counts.poll(), counts.peek());
 
 			index--;
 		}
 		return count;
+	}
+
+	public int countBinarySubstringsAlt(String s) {
+		int ans = 0, prev = 0, cur = 1;
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i-1) != s.charAt(i)) {
+				ans += Math.min(prev, cur);
+				prev = cur;
+				cur = 1;
+			} else {
+				cur++;
+			}
+		}
+		return ans + Math.min(prev, cur);
 	}
 }
