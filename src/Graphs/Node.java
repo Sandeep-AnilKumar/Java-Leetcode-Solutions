960a882f0fe705fa5d54f4ad0c56f131ec34ca97
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-12-04 11:59:29 -0800

    New Code (Added Graph Node implementation)

---
+++ src/Graphs/Node.java
@@ -0,0 +1,58 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sandeepa
+ */
+
+public class Node {
+	private static enum State {
+		Unvisited, Visiting, Visited;
+	}
+
+	private int val;
+	private State state;
+	private List<Node> adjacent;
+
+	public Node(int val) {
+		this.val = val;
+		this.state = State.Unvisited;
+		this.adjacent = new ArrayList<Node>();
+	}
+
+	public int getValue() {
+		return this.val;
+	}
+
+	public void setValue(int val) {
+		this.val = val;
+		return;
+	}
+
+	public void addAdjacentNode(Node node) {
+		this.adjacent.add(node);
+		return;
+	}
+
+	public List<Node> getAdjacentNodes() {
+		return this.adjacent;
+	}
+
+	public State getState() {
+		return this.state;
+	}
+
+	public void setVisiting() {
+		this.state = State.Visiting;
+	}
+
+	public void setVisited() {
+		this.state = State.Visited;
+	}
+
+	public void setUnvisited() {
+		this.state = State.Unvisited;
+	}
+}
