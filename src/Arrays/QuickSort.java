6cab5c203158342856b5f120d5b17384ef798567
Sandeep Anil Kumar <sandeepkanil@gmail.com>
2017-12-18 11:42:58 -0800

    New Version (Sorting)

--- src/Arrays/QuickSort.java
+++ src/Arrays/QuickSort.java
@@ -1,50 +1,51 @@
 package Arrays;
 
 public class QuickSort {
-    static int[] array = {5,3,-4,9,-9,2,4,-2};
-    public static void main(String[] args) {
-        System.out.println("Quick Sort Start");
-        quickSort(array, 0, array.length-1);
-        printArray();
-        System.out.println("\nQuick Sort End");
-    }
+	static int[] array = {5, 5, 5, 5, 6, 5};
+	public static void main(String[] args) {
+		System.out.println("Quick Sort Start");
+		quickSort(array, 0, array.length-1);
+		printArray();
+		System.out.println("\nQuick Sort End");
+	}
 
-    private static void printArray() {
-        for(int i : array){
-            System.out.print(i+" ");
-        }
-    }
+	private static void printArray() {
+		for(int i : array){
+			System.out.print(i+" ");
+		}
+	}
 
-    public static void quickSort(int[]A, int low, int high){
-        int pivot;
-        if(high>low) {
-            pivot = partition(A,low,high);
-            quickSort(A, low, pivot-1);
-            quickSort(A, pivot+1, high);
-        }
-    }
+	public static void quickSort(int[]A, int low, int high){
+		int pivot;
+		if(high > low) {
+			pivot = partition(A,low,high);
+			quickSort(A, low, pivot - 1);
+			quickSort(A, pivot + 1, high);
+		}
+	}
 
-    private static int partition(int[] A, int low, int high) {
-        int left, right, pivot_item = 0;
-        left = low;
-        right = high;
-        while(left<right) {
-            while(A[left]<=pivot_item)
-                left++;
-            while(A[right]>pivot_item)
-                right--;
-            if(left<right)
-                swap(A,left,right);
-        }
-        A[low] = A[right];
-        A[right] = pivot_item;
-        return right;
-    }
+	private static int partition(int[] A, int low, int high) {
+		int left, right, pivot = A[low];
+		left = low;
+		right = high;
+		while(left < right) {
+			while(A[left] <= pivot)
+				left++;
+			while(A[right] > pivot)
+				right--;
+			if(left < right)
+				swap(A,left,right);
+		}
+		A[low] = A[right];
+		A[right] = pivot;
+		return right;
+	}
 
-    private static void swap(int[]A, int left, int right){
-        int temp = 0;
-        temp = A[left];
-        A[left] = A[right];
-        A[right] = temp;
-    }
+	private static void swap(int[]A, int left, int right){
+		if(left != right) {
+			A[left] = A[left] ^ A[right];
+			A[right] = A[left] ^ A[right];
+			A[left] = A[left] ^ A[right];
+		}
+	}
 }
